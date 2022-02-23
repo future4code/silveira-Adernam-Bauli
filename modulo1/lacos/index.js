05333c0bfa87b0a78326ba1108707cffcc2f6c40
Baulii <adernam147@hotmail.com>
2022-02-23 16:09:22 -0400

    Exercicios laçoas

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,117 @@
+// Exercícios de interpretação de código ----------------------------------
+
+// 1) 10
+
+// 2)
+
+// a.
+
+// 19
+// 21
+// 23
+// 25
+// 27
+// 30
+
+// b.
+
+// let indice = 0
+// const lista = [10, 11, 12, 15]
+// for(let numero of lista){
+
+//     if (numero > 18){
+//         console.log(numero, "indice", indice)
+//     }
+
+//     indice++
+// }
+
+// 3) "****"
+
+// Exercícios de escrita de código ----------------------------------------
+
+// 1)
+
+// const bichoEstimacao = +prompt("Quantos bichos de estimacao você tem :");
+// let bichos = []
+
+// if (bichoEstimacao < 1){
+//     console.log("Que pena! Você pode adotar um pet!")
+// } else {
+//     const nomesBichos =prompt("Quais os nomes deles ?")
+//     bichos.push(nomesBichos)
+// }
+// console.log(bichos)
+
+// 2)
+
+// a.
+
+// const arrayOriginal = [10, 20, 50, 99, 100,]
+
+// function imprimir (num){
+//     console.log(num)
+// }
+
+// imprimir(arrayOriginal)
+
+// b.
+
+// function dividir (){
+//     console.log(arrayOriginal[0] / 10)
+//     console.log(arrayOriginal[1] / 10)
+//     console.log(arrayOriginal[2] / 10)
+//     console.log(arrayOriginal[3] / 10)
+// }
+
+// dividir(arrayOriginal)
+
+// c.
+
+// for(let number of arrayOriginal){
+
+//     if (number % 2 == 0) {
+//         console.log(number)
+//     }
+
+// }
+
+// d.
+
+// let pares = 0;
+// let par = 0;
+// for(let par of arrayOriginal){
+//     if ((par % 2) == 0) {
+//         pares = par;
+//         console.log(par)
+//     }
+
+// }
+
+
+
+// e.
+
+// let maior = 0;
+// for(let num of arrayOriginal){
+
+//     if (num > maior) {
+//         maior = num;
+//     }
+
+// }
+
+// console.log("O número maior é:", maior);
+
+
+
+// let menor = arrayOriginal[0];
+// for(let num1 of arrayOriginal){
+
+//     if (num1 < menor) {
+//         menor = num1;
+//     }
+
+// }
+
+// console.log("O número menor é:", menor);
