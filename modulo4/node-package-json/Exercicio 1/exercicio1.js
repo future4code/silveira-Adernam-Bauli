93fd41bde5bc7be8a1b595f44b20f031a727f990
Baulii <adernam147@hotmail.com>
2022-05-25 15:54:47 -0400

    intro typescript

---
+++ modulo4/node-package-json/Exercicio 1/exercicio1.js
@@ -0,0 +1,9 @@
+const myName = prompt("Qual é o seu nome ?")
+
+const age = Number(prompt("Qual sua idade ?"))
+
+console.log(`Olá, ${myName}! Você tem ${age} anos.`)
+
+const postAge = age + 7
+
+console.log(`Olá, ${myName}! Você tem ${age} anos. Em sete anos você terá ${postAge}`)
---
+++ modulo4/node-package-json/Exercicio 2/exercicio2.js
@@ -0,0 +1,7 @@
+const val1 = Number(process.argv[2])
+
+const val2 = Number(process.argv[3])
+
+const sum = val1 + val2
+
+console.log(sum)
