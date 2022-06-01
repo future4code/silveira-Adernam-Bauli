1a75aeb0af995763cef988fef49615cf0f6e9015
Baulii <adernam147@hotmail.com>
2022-06-01 15:22:01 -0400

    fluxo-de-dados-backend

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,17 @@
+export const arrayProducts = [
+    {
+        id: "1",
+        name: "celular",
+        price: 5000
+    },
+    {
+        id: "2",
+        name: "tablet",
+        price: 1000
+    },
+    {
+        id: "3",
+        name: "computador",
+        price: 8000
+    }
+]
